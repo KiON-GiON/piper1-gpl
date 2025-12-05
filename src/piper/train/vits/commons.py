82c687db2e4a997b5877fa0cb022874c828f7086
unknown <KiON-GiON@users.noreply.github.com>
2025-12-05 04:56:45 -0300

    Undo changes

--- src/piper/train/vits/commons.py
+++ src/piper/train/vits/commons.py
@@ -44,12 +44,10 @@ def rand_gumbel_like(x):
     return g
 
 
-def slice_segments(x, ids_str, segment_size):
-    # x: [B, C, T]
-    b, c, t = x.size()
-    ret = torch.zeros(b, c, segment_size, dtype=x.dtype, device=x.device)
-    for i in range(b):
-        idx_str = ids_str[i]
+def slice_segments(x, ids_str, segment_size=4):
+    ret = torch.zeros_like(x[:, :, :segment_size])
+    for i in range(x.size(0)):
+        idx_str = max(0, ids_str[i])
         idx_end = idx_str + segment_size
         ret[i] = x[i, :, idx_str:idx_end]
     return ret
