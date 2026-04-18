1a76f7891109434c88f9bf05baadcde47a3d8d07
unknown <KiON-GiON@users.noreply.github.com>
2026-04-18 10:03:34 -0400

    .

--- src/piper/train/vits/transforms.py
+++ src/piper/train/vits/transforms.py
@@ -170,10 +170,9 @@ def rational_quadratic_spline(
         )
         c = -input_delta * (inputs - input_cumheights)
 
-        discriminant = b.pow(2) - 4 * a * c
-        assert (discriminant >= 0).all(), discriminant
-
-        root = (2 * c) / (-b - torch.sqrt(discriminant))
+            discriminant = b.pow(2) - 4 * a * c
+            discriminant = torch.clamp(discriminant, min=0.0)
+            root = torch.sqrt(discriminant + 1e-9)
         outputs = root * input_bin_widths + input_cumwidths
 
         theta_one_minus_theta = root * (1 - root)
